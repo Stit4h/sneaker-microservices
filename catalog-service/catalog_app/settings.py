47f31187416d80da9a56bd184a83d5fbc82ec093
Andrey Nikitin <dyka.nikitin.2019@mail.ru>
2026-05-19 15:42:22 +0300

    переключил на SQLite для Render

--- catalog-service/catalog_app/settings.py
+++ catalog-service/catalog_app/settings.py
@@ -72,14 +72,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'catalog_app.wsgi.application'
 
+# Используем SQLite для Render (бесплатный PostgreSQL недоступен в одном контейнере)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'sneaker_db'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_HOST', 'postgres'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
